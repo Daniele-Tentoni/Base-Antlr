75765d42d99c3bb11520ad76e862681eaa5f3478
Daniele Tentoni <d.tentoni@wedoit.io>
2020-12-06 22:29:11 +0100

    Javadoc documentation.

--- src/main/java/fool/FOOLObjectFactory.java
+++ src/main/java/fool/FOOLObjectFactory.java
@@ -1,20 +1,22 @@
 package fool;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.net.URL;
-import java.util.Objects;
 import lcmc.ObjectFactory;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Objects;
 
 /**
- * Provide factory methods for antlr objects for FOOL language.
+ * Provide factory methods for Antlr objects for FOOL language.
  */
 public final class FOOLObjectFactory implements
     ObjectFactory<fool.FOOLLexer, fool.FOOLParser> {
-
+  /**
+   * This is the singleton instance of the factory.
+   */
   private static FOOLObjectFactory instance;
 
   /**
@@ -36,11 +38,16 @@ public final class FOOLObjectFactory implements
   }
 
   /**
-   * Get the singleton FOOLLexer instance.
+   * Create a new FOOLLexer instance.
    *
-   * @param fileName that lexer have to read.
+   * @param fileName that lexer have to read. Must be in the resource
+   *                 directory of the project. Modify him after the lexer
+   *                 generation affect him, because the stream reading happen
+   *                 when you call methods on FOOL Lexer instance.
    * @return lexer singleton instance.
-   * @throws IOException when file is not found.
+   * @throws IOException when file is not found. Check the path to the
+   *                     resource path of the project. If you call him from test classes a file
+   *                     must be in the resource path of test, not of the source.
    */
   @Override
   public fool.FOOLLexer getLexer(final String fileName) throws IOException {
@@ -55,6 +62,7 @@ public final class FOOLObjectFactory implements
 
   /**
    * Get the singleton FOOLParser instance.
+   * Calling this method make lexer trigger.
    *
    * @param foolLexer lexer that produce tokens.
    * @return parser that create syntax trees.
